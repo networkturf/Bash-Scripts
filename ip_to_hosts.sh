c272c4e7ae3b3599a328d1257401eaa5db501ca4
networkturf <networkturf@gmail.com>
2018-05-27 20:24:30 -0700

    added comments to ths file

--- ip_to_hosts.sh
+++ ip_to_hosts.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-cat ip_list.txt | while read LINE
+cat ip_list.txt | while read LINE   #This iterates over the file ip_ist.xtx which should contain IP addresses line by line
 do
-host $LINE | awk '{print $5}' | sed 's/.$//'
+host $LINE | awk '{print $5}' | sed 's/.$//'   # The 'host' command runs against each IP address  
 done
 
